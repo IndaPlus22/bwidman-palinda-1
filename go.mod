7fbe600648fabb74fe6f6dcea1b3b850768cafbc
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-03-20 19:19:31 +0100

    Slices exercise

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/IndaPlus22/bwidman-palinda-1
+
+go 1.20
+
+require golang.org/x/tour v0.1.0
--- src/gotour_exercises/loops.go
+++ src/gotour_exercises/loops.go
@@ -7,7 +7,7 @@ import (
 
 func Sqrt(x float64) float64 {
 	z := x / 2.0
-	z_prev := x + 1
+	z_prev := z + 1
 	for i := 0; math.Abs(z - z_prev) > 0.000000000000001; i++ {
 		z_prev = z
 		z -= (z*z - x) / (2*z)
